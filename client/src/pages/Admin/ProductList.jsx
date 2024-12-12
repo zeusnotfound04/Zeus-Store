ac18bfb80e6a4ac1972c2a5a7f9f404405f263a8
zeusnotfound04 <zeusnotfound0@gmail.com>
2024-12-12 22:47:45 +0530

    fixed a bug from product list section

--- client/src/pages/Admin/ProductList.jsx
+++ client/src/pages/Admin/ProductList.jsx
@@ -44,6 +44,74 @@ const ProductList = () => {
             </div>
           )}
 
+          <div className='mb-3'>
+            <label className='border text-white px-4 black w-full text-center rounded-lg cursor-pointer font-bold py-11'>
+              {imageUrl ? "Change Image" : "Upload Image"}
+              <input 
+                type="file" 
+                name='image'
+                accept='image/*'
+                className={!image ? 'hidden' : 'text-white'}
+                onChange=""
+                
+                />
+            </label>
+
+          </div>
+
+
+            <div className='p-3'>
+              <div className='flex flex-wrap'>
+                <div className='one'>
+                  <label htmlFor='name block'>Name</label>
+                    <input 
+                      type="text" 
+                      name='name'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className='border mb-3 bg-[#101011] w-[30rem] p-4 rounded-lg text-white'
+                      />
+
+                </div>
+                <div className='two ml-10'>
+                  <label htmlFor='name block'>Price</label>
+                    <input 
+                      type="text" 
+                      name='price'
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                      className='border mb-3  bg-[#101011] w-[30rem] p-4 rounded-lg text-white'
+                      />
+
+                </div>
+              </div>
+
+              <div className='flex flex-wrap'>
+                <div className='one'>
+                  <label htmlFor='name block'>Quantity</label>
+                    <input 
+                      type="number" 
+                      name='quantity'
+                      value={quantity}
+                      onChange={(e) => setQuantity(e.target.value)}
+                      className='border mb-3 bg-[#101011] w-[30rem] p-4 rounded-lg text-white'
+                      />
+
+                </div>
+                <div className='two ml-10'>
+                  <label htmlFor='name block'>Brand</label>
+                    <input 
+                      type="text" 
+                      name='brand'
+                      value={price}
+                      onChange={(e) => setBrand(e.target.value)}
+                      className='border mb-3  bg-[#101011] w-[30rem] p-4 rounded-lg text-white'
+                      />
+
+                </div>
+              </div>
+
+            </div>
 
         </div>
 
