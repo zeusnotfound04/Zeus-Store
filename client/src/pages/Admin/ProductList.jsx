80e416d75e56fbe3f57d476b1ce5ca1766f352fe
zeusnotfound04 <zeusnotfound0@gmail.com>
2024-12-12 22:31:47 +0530

    working on product list section

--- client/src/pages/Admin/ProductList.jsx
+++ client/src/pages/Admin/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router'
 import {useGetProductsQuery , 
     useGetProductByIdQuery , 
@@ -13,8 +13,45 @@ import {useGetProductsQuery ,
     useUploadProductImageMutation} from '../../redux/api/productApiSlice'
 
 const ProductList = () => {
+
+  const [image , setImage] = useState("")
+  const [name , setName] = useState("")
+  const [description , setDescription] = useState("")
+  const [price , setPrice] = useState("")
+  const [category , setCategory] = useState("")
+  const [quantity , setQuantity] = useState("")
+  const [brand, setBrand] = useState("")
+  const [stock , setStock] = useState(0)
+  const [imageUrl , setImageUrl] = useState(null)
+
+  const navigate = useNavigate()
+
+  const [uploadProductImage] = useUploadProductImageMutation()
+  const [createProduct] = useCreateProductMutation()
+  const { data: products } = useGetProductsQuery()
+
   return (
-    <div>ProductList</div>
+
+    <div className='container xl:mx-[9rem] sm:mx-[0]'>
+      <div className='flex flex-col md:flex-row'>
+        {/* <AdminMenu/> */}
+        <div className='md:w-3/4 p-3'>
+          <div className='h-12'>Create Product</div>
+
+          {imageUrl && (
+            <div className='text-center'>
+              <img src={imageUrl} alt="product" className='block mx-auto max-h-[200px]'/>
+            </div>
+          )}
+
+
+        </div>
+
+      </div>
+
+
+    </div>
+     
   )
 }
 
